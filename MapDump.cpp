e7237c804f2d39089ae9be2957565e316395df44
Zhijie Li <27743673+zhijie-li@users.noreply.github.com>
2018-09-06 15:38:08 -0400

    Add files via upload
    -bin 2  : simple 2x2 binning by adding adjacent pixels together before rescaling
    -statframe 20 : use first 20 frames to recalcalculate min, max, rms, mean
    Now it is also possible to accept more than one .mrcs files in the command line, thereby enabling wildcards

--- MapDump.cpp
+++ MapDump.cpp
@@ -14,17 +14,23 @@
 
 
 
+int findparam(int argc,char *argv[], char *infile[], char *outdir, char *prefix,  int * statframe, int * bin_factor); //find the input file list
+
+
 void statarray(float * ary, int size,float *min,float *max,float *rms,float *mean);
 
-int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int *r, int *s , float *amin, float *amax, float *amean, float *arms);
+
+int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int *r, int *s , float *amin, float *amax, float *amean, float *arms, int * mode, int * datasize);
 
 
 int readmap(const char *filename0, float *array, int nsymbt, int c, int r, int s);
 
-int read_slice(const char *filename0, float *array,int nsymbt,int c, int r,int slice) ;//slice start from 0
+int read_whole_array(const char *filename0, float *array,int nsymbt,int c, int r,int s, int datasize) ;//read the whole array (1.6G for K2 in real4)
+int read_slice_float      (const char *filename0, float *array,int nsymbt,int c, int r,int slice, int datasize) ;//slice start from 0
+int read_slice_char      (const char *filename0, char *array,int nsymbt,int c, int r,int slice, int datasize) ;//slice start from 0
 int dumparray(const char *filename0, char *png, int arraysize);
 
-int bin(float * array, float * array_bin,int x, int y);
+int bin(float * array, float * array_bin,int x, int y, int bin_factor);
 
 //int addtoheader(char * out_header, float max,float min,float mean,float rms, int num_map);
 //int addtoheader(char * out_header, float max,float min,float mean,float rms, char * label);
@@ -47,47 +53,97 @@ int main(int argc,char *argv[])
 { 
   float t1,t2;
   t1=clock();
-  printf ("\nMapDump: dump CCP4/MRC map/movie/image files to PNG.\n\n");
+  printf ("\nMapDump: dump CCP4/MRC map/movie/image files to PNG.\n Input must have .mrcs extension\n\n");
+
+//parse input
+  char **infile = new char*[2048]; 
+  char *outdir=new char[1024];
+  char *prefix=new char[1024];
+  for (int i = 0; i < 2048; ++i) {    infile[i] = new char[1024];  }
+	int statframe=0;
+	int bin_factor=0;
+  int num_infile= findparam(argc,argv, infile, outdir, prefix, &statframe, &bin_factor);
+  std::cout<<"Num of files to dump: "<<num_infile<<'\n';
+	for(int f=0;f<num_infile;f++){
+		printf("%d %s\n",f+1, infile[f]);
+	}
+  std::cout<<"Num of slices used for statistics: "<<statframe<<'\n'<<'\n';
+  std::cout<<"Binning: "<<bin_factor<<'\n'<<'\n';
+
+//
+	for(int f=0;f<num_infile;f++){
 
   char *header=new char[1024];
   char *symmop=new char[10240];//more than enough
+  
   
     int   c=0,r=0,s=0;
     int   nsymbt=0;
 
     char * fn=new char[1024];
-    strcpy(fn,argv[1]);
+    strcpy(fn, infile[f]);
 
 
     float amin=0,amax=0,amean=0,arms=0;
-    readmap_header(const_cast<char*>(fn), header, &nsymbt, &c,&r,&s,&amin,&amax,&amean,&arms);//get size of the first file
-
+    int datasize=4; //default real4
+    int mode=2; //default
+    readmap_header(const_cast<char*>(fn), header, &nsymbt, &c,&r,&s,&amin,&amax,&amean,&arms, &mode, &datasize);//get size of the first file
+		if (arms <=0 && statframe==0){
+			printf("\nThe RMS found in header is zero or negative. updatinging with frame 0\n");
+			statframe=1;
+			}
 
     int arraysize=c*r*s;
     int slice_size=c*r;
-    float *array    =new float[slice_size];
+		if (mode ==2){;		}
+		else{
+			printf("map mode ERROR: %d\n",mode);
+			exit(0);
+			}
 
-    float *array_bin =new float[slice_size/4];
-    float *array_bin2 =new float[slice_size/16];
-    float *array_bin3 =new float[slice_size/64];
+    	float **array    =new float * [s];
+    	for(int z=0;z<s;z++){ 
+	  	 	array[z]    =new float [slice_size];
+	      read_slice_float(const_cast<char*>(fn), array[z],nsymbt,c,r,z, datasize); //read one slice
+	  	}
+
+		/*if (mode ==0){
+    	float **array    =new char * [s];
+    	for(int z=0;z<s;z++){ 
+	  	 	array[z]    =new char [slice_size];
+	      read_slice_char(const_cast<char*>(fn), array[z],nsymbt,c,r,z, datasize); //read one slice
+	  	}
+		}
+	  */
     
-    
+              float amin_n=0;
+              float amax_n=0;
+              float arms_n=0;
+              float amean_n=0;
+		int statcont=0;
+    for(int z=0;z<s && z<statframe;z++){ //statframe is upper limit of the frames to quant
 
+              printf ("\n\nUpdating stats using slice %d....\n\n",z);
+              float amin_s;
+              float amax_s;
+              float arms_s;
+              float amean_s;
+              statarray(array[z],c*r,&amin_s,&amax_s,&arms_s,&amean_s);
+              amin_n+=amin_s;
+              amax_n+=amax_s;
+              arms_n+=arms_s;
+              amean_n+=amean_s;
+              statcont++;
 
-
-
-
-    for(int z=0;z<s;z++){
-
-        read_slice(const_cast<char*>(fn), array,nsymbt,c,r,z); //read one slice
-
-        if(z==0 ){ //need to updata rms based on slice 1
-                printf ("\n\nUpdating stats using slice %d....\n\n",z);
-              statarray(array,c*r,&amin,&amax,&arms,&amean);
             }
+              amin_n/=statcont;
+              amax_n/=statcont;
+              arms_n/=statcont;
+              amean_n/=statcont;
 
 
-        
+
+    for(int z=0;z<s;z++){     
         char  png_name[1024];
 
         float lowcut=amean-6*arms; //default
@@ -102,33 +158,44 @@ int main(int argc,char *argv[])
 
         
         
-        rescale(array,scale,lowcut,c*r);
         
             
-        //  array_bin=array;      
-        //bin(array,array_bin,c,r); 
-         //bin(array_bin,array_bin2,c/2,r/2); 
-         //bin(array_bin2,array_bin3,c/4,r/4);
          char title[1024]; 
          
          printf ("saving image to file <%s>\n", png_name);
-        writeImage(png_name, c,r, array);
-        
 
+        if(bin_factor<=0){
+					rescale(array[z],scale,lowcut,c*r);
+					writeImage(png_name, c,r, array[z]);
+					}
+        	else{
+		          float * array_bin=new float [(c/bin_factor)*(r/bin_factor)];      
+
+	        		bin(array[z],array_bin,c,r,bin_factor); 
+	        		std::cout<<"ok\n";
+							rescale(array_bin,scale/bin_factor/bin_factor,lowcut,(c/bin_factor)*(r/bin_factor));
+							std::cout<<(c/bin_factor)*(r/bin_factor)<<"\n";
+							writeImage(png_name, c/bin_factor,r/bin_factor, array_bin);
+							delete array_bin;
+        		}
+        
+				delete array[z];
         }
 
-//    dumparray("dump.ary", png,  arraysize);
+
+	//    dumparray("dump.ary", png,  arraysize);
     
 
-  delete array;
+	  delete array;
 
-
+	}//end of file cycle
+	
   t2 = clock() - t1;
   printf ("%f seconds.\n",((float)t2)/CLOCKS_PER_SEC);
 	return 0;
 }
 
-int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int *r, int *s , float *amin, float *amax, float *amean, float *arms)
+int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int *r, int *s , float *amin, float *amax, float *amean, float *arms, int *mode, int *dtsz)
 {
   int fsize=0;
   std::ifstream in (filename0,std::ios::in|std::ios::binary|std::ios::ate);
@@ -142,7 +209,17 @@ int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int
     in.read(( char*)c,4); 
     in.read(( char*)r,4);
     in.read(( char*)s,4);
+    in.read(( char*)mode,4);
+
+		if(*mode == 0)    {*dtsz=1;}
+		if(*mode == 1)    {*dtsz=2;}
+		if(*mode == 2)    {*dtsz=4;}
+		if(*mode == 3)    {*dtsz=4;}
+		if(*mode == 4)    {*dtsz=8;}
+		if(*mode == 6)    {*dtsz=2;}
+			
     printf("x y z: %d %d %d\n",*c,*r,*s);
+    printf("mode: %d,  %d Bytes per pixel\n",*mode,*dtsz);
 
     int arraysize=(*c)*(*r)*(*s);
 
@@ -152,7 +229,7 @@ int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int
     in.read(( char*)amean,4);
     in.seekg (216, std::ios::beg);
     in.read(( char*)arms,4); 
-
+		
     printf("min %8.3f  max %8.3f  mean %8.3f  RMS %8.3f\n",*amin,*amax,*amean,*arms);
 
     
@@ -163,9 +240,9 @@ int readmap_header(const char *filename0, char *header, int *nsymbt, int *c, int
     printf("SYMM bytes: %d\n",*nsymbt);
 
 
-    printf( "Array size: %d * 4 (float32) \nSYMM block bytes: nsymbt_str %d \n\n",arraysize,*nsymbt);
-
-    if(arraysize*4+1024+*nsymbt != fsize){      printf ("error! file size %d does not equal to arraysize (%d) + 1024 + %d \n!!", fsize, arraysize,*nsymbt);      }
+    printf( "Array size: %d \nSYMM block bytes: nsymbt_str %d \n\n",arraysize,*nsymbt);
+		int arraysize_dt=arraysize * (*dtsz);
+    if( arraysize_dt +1024+*nsymbt != fsize){      printf ("error! file size %d does not equal to data array size (%d) + 1024 + %d \n!!", fsize, arraysize_dt,*nsymbt);      }
 
   }
   in.close();
@@ -199,7 +276,8 @@ int readmap(const char *filename0, float *array,int nsymbt,int c, int r, int s)
   return fsize;
 }
 
-int read_slice(const char *filename0, float *array,int nsymbt,int c, int r,int slice) //slice start from 0
+
+int read_whole_array(const char *filename0, float *array,int nsymbt,int c, int r,int s, int datasize) //slice start from 0
 {
   int fsize=0;
 
@@ -208,17 +286,62 @@ int read_slice(const char *filename0, float *array,int nsymbt,int c, int r,int s
 
   if (in.is_open()){
     fsize = in.tellg();
-    int start=1024+nsymbt+(slice)*c*r*4;
-    int end=1024+nsymbt+(slice+1)*c*r*4;
+    int start=1024+nsymbt;
+    int end=1024+nsymbt+s*c*r*datasize;
+    if(end>fsize){      printf ("error! file size %d <  \n!!", fsize);      }
+        printf ("reading the data array of (%d)bytes x %d x %d, %d frames at bytes %12d - %12d ... ",datasize, c,r,s,start,end);
+
+    in.seekg (start, std::ios::beg);
+    in.read(( char *)array,(c*r*s*datasize));
+  }
+
+  in.close();
+  return c*r*s*datasize;
+}
+
+
+int read_slice_float(const char *filename0, float *array,int nsymbt,int c, int r,int slice ,int datasize) //slice start from 0
+{
+  int fsize=0;
+
+  std::ifstream in (filename0,std::ios::in|std::ios::binary|std::ios::ate);
+	if(!in)	{	  printf ( "file does not exist\n");    return 0;  }
+
+  if (in.is_open()){
+    fsize = in.tellg();
+    int start=1024+nsymbt+(slice)*c*r*datasize;
+    int end=1024+nsymbt+(slice+1)*c*r*datasize;
+    if(end>fsize){      printf ("error! file size %d <  \n!!", fsize);      }
+        printf ("reading slice at bytes %12d - %12d ... \n",start,end);
+
+    in.seekg (start, std::ios::beg);
+    in.read(( char *)array,(c*r*datasize));
+  }
+
+  in.close();
+  return c*r*datasize;
+}
+
+int read_slice_char(const char *filename0, char *array,int nsymbt,int c, int r,int slice ,int datasize) //slice start from 0
+{
+  int fsize=0;
+
+  std::ifstream in (filename0,std::ios::in|std::ios::binary|std::ios::ate);
+	if(!in)	{	  printf ( "file does not exist\n");    return 0;  }
+
+  if (in.is_open()){
+    fsize = in.tellg();
+    int start=1024+nsymbt+(slice)*c*r*datasize;
+    int end=1024+nsymbt+(slice+1)*c*r*datasize;
     if(end>fsize){      printf ("error! file size %d <  \n!!", fsize);      }
         printf ("reading slice at bytes %12d - %12d ... ",start,end);
 
     in.seekg (start, std::ios::beg);
-    in.read(( char *)array,(c*r*4));
+    in.read(( char *)array,(c*r*datasize));
   }
 
   in.close();
-  return c*r*4;
+  return c*r*datasize;
 }
 
 int dumparray(const char *filename0, char *png, int arraysize)
@@ -247,7 +370,7 @@ void statarray(float * ary, int size,float *min,float *max,float *rms,float *mea
     for (int i=0;i<size; i++){
         if(*min>*(ary+i)){*min=*(ary+i);}
         if(*max<*(ary+i)){*max=*(ary+i);}
-        *mean+=*ary;
+        *mean+=*(ary+i);
     }
     *mean=*mean/size;
     
@@ -330,16 +453,24 @@ void statarray(float * ary, int size,float *min,float *max,float *rms,float *mea
 //
 
 
-int bin(float * array, float * array_bin,int x, int y)
+int bin(float * array, float * array_bin,int x, int y, int bin_factor)
 {
-    for (int i=0;i<y/2;i++){
-        for (int j=0;j<x/2;j++){
-            int idx=i*x/2+j;
-            *(array_bin+idx)=(*(array+i*2*x+j*2)+*(array+(i*2+1)*x+j*2+1)+*(array+(i*2+1)*x+j*2)+*(array+(i*2)*x+j*2+1))*4;
-            
-        
+
+	int idx=0;
+    for (int i=0;i<y-y%bin_factor;i=i+bin_factor){
+        for (int j=0;j<x-x%bin_factor;j=j+bin_factor){ //each element in new array
+        	*(array_bin+idx)=0;
+            	for (int yy=i;yy<i+bin_factor;yy++)
+            		{
+            			for (int xx=j;xx<j+bin_factor;xx++)
+            			{
+            				*(array_bin+idx)	+= *(array+xx+x*(yy));
+            			}
+								}
+	       	idx++;
         }
     }
+    std::cout<<idx<<"\n";
 } 
 int rescale(float * ary, float scale,float min, int size){
     printf(" rescaling with min = %f scale = %f ...\n",min,scale);
@@ -440,4 +571,95 @@ int writeImage(const char* filename, int width, int height, float *buffer)
 	if (row != NULL) delete row;
 
 	return code;
+}
+
+
+
+
+
+
+//modified from map_average_gcc.cpp
+
+
+
+int findparam(int argc,char *argv[], char *infile[], char *outdir, char *prefix, int * statframe, int * bin_factor) //find the input file list
+{
+  char *dir=NULL;
+  char *filelist=NULL;
+  char *ext=new char[1024]; strcpy(ext,".mrc");
+  int file_count=0;
+  int dir_sig=0;  
+  int fl_sig=0;
+  
+  
+ 	for (int i=1;i<argc;i++)
+	{
+		//printf ("%d %d %s\n",argc,i,argv[i]);
+	  std::string current=argv[i];
+	  
+ 	  if(strcmp(argv[i],"-d")==0  && i<argc-1) { dir_sig=1;      dir=argv[i+1]; i+=1;continue;}
+ 	  if(strcmp(argv[i],"-l")==0  && i<argc-1) { fl_sig=1;  filelist=argv[i+1]; i+=1;continue;}
+ 	  if(strcmp(argv[i],"-o")==0  && i<argc-1) {      strcpy(outdir,argv[i+1]); i+=1;continue;}
+ 	  if(strcmp(argv[i],"-prefix")==0  && i<argc-1) { strcpy(prefix,argv[i+1]); i+=1;continue;}
+ 	  if(strcmp(argv[i],"-statframe")==0  && i<argc-1) { 
+ 	  	char *tmp= new char[100]; 
+ 	  	strcpy(tmp,argv[i+1]);
+ 	  	*statframe=std::__cxx11::stoi(tmp);
+// 	  	printf ("stat : %s", tmp); 
+			i+=1;continue;
+		}
+ 	  if(strcmp(argv[i],"-bin")==0  && i<argc-1) { 
+ 	  	char *tmp= new char[100]; 
+ 	  	strcpy(tmp,argv[i+1]);
+ 	  	*bin_factor=std::__cxx11::stoi(tmp);
+			i+=1;continue;
+		}
+
+ 	  if ( current.length()>4 && (current.length()-current.rfind(ext) ==4 || current.length()-current.rfind(ext) ==5 )) 
+ 	  { 
+       strcpy ( infile[file_count] ,argv[i] );
+       file_count++;
+//       if(file_count>=2048){printf ("cannot handle more than 2048 files\n");return file_count;}
+    }
+  }
+
+  if(fl_sig==1)
+    {
+      std::string  line;
+      std::ifstream in (filelist);
+      if (in.is_open())
+      {
+        while ( getline (in,line) )
+        {
+ 	        if ( line.length()>4 && line.length()-line.rfind(ext) ==4) 
+ 	        { 
+            strcpy ( infile[file_count] , (char*)line.c_str() );
+            file_count++;
+        if(file_count>=2048){printf ("cannot handle more than 2048 files\n");return file_count;}
+          }
+        }
+      in.close();
+      }
+    }
+
+
+  if(dir_sig==1)
+    {
+      DIR *dir1;
+      struct dirent *ent;
+      if ((dir1 = opendir (dir)) != NULL) {
+        while ((ent = readdir (dir1)) != NULL) {
+          std::string line=ent->d_name;
+ 	        if ( line.length()>4 && line.length()-line.rfind(ext) ==4) 
+ 	        { 
+            strcpy ( infile[file_count] , (char*)line.c_str() );
+            file_count++;
+            if(file_count>=2048){printf ("cannot handle more than 2048 files\n");return file_count;}
+          }
+        }
+      closedir (dir1);
+      } 
+    }
+
+  return file_count;
 }
